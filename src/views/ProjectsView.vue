5381448e209fe80fd135aea5112e1cf8be35f48e
Alex <alexfreyjr@gmail.com>
2023-10-03 12:55:50 +0200

    change carousel to cycleList

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -1,43 +1,23 @@
 <script setup>
-import { Carousel, Navigation, Slide } from 'vue3-carousel'
-import 'vue3-carousel/dist/carousel.css'
-
-const slides = [
-  {
-    id: 1,
-    title: 'Project HRNet',
-    content: '../assets/hrnet.png'
-  },
-  {
-    id: 2,
-    title: 'Date Picker',
-    content: '../assets/datepicker.png'
-  },
-  {
-    id: 3,
-    title: 'Table',
-    content: '../assets/table.png'
-  },
-]
+import { useCycleList } from '@vueuse/core'
+const { state, prev, next } = useCycleList([
+  '/src/assets/project/hrnet.png',
+ '/src/assets/project/datepicker.png',
+ '/src/assets/project/table.png'
+]);
 </script>
 
 <template>
   <div class="Projects">
     <div class="Project"> 
       <h2>Project HRNet</h2>
-      <Carousel>
-        <Slide v-for="slide in slides" :key="slide.id">
-          <div class="carousel__item">
-            <a :href="slide.content" target="_blank">
-              <img class="project_img" :src="slide.content" :alt="slide.title" />
+      <div class="carousel">
+          <img src='../assets/left.png' @click="prev()" alt="left" class="leftArrow"/>
+            <a :href="state" target="_blank">
+              <img class="project_img" :src="state" alt="hrnet"/>
             </a>
-          </div>
-        </Slide>
-        <template #addons>
-          <Navigation />
-          <Pagination />
-        </template>
-      </Carousel>
+          <img src="../assets/right.png" @click="next()" alt="right" class="rightArrow"/>
+      </div>
       <p class="blurb">The goal of this project was to recreate an existing Jquery page in React.<br />
         For this I've selected a few components to implement and develop the date picker from scratch.<br />
         it's available here: <a href="https://www.npmjs.com/package/date-picker-typescript">date-picker-typescript </a>
@@ -51,7 +31,7 @@ const slides = [
     <div class="Project"> 
       <h2>Project Sportsee</h2>
       <a href="/assets/Sportsee.png" target="_blank">
-        <img class="project_img" src="../assets/Sportsee.png" alt="Sportsee" />
+        <img class="project_img" src="../assets/project/Sportsee.png" alt="Sportsee" />
       </a>
       <p class="blurb">The goal of this project was to create a dashboard connect to an existing API.<br />
         For this I've used React and Recharts.js for all the charts.<br />
@@ -76,18 +56,27 @@ const slides = [
  margin-right: 2rem;
  margin-top: 10%;
 }
-
-
 .project_img {
     width: 100%;
     height: 450px;
     object-fit: contain;
-    border-radius: 20px;
     margin-top: 1rem
 }
 .little-icon{
   width: 50px;
   height: 50px;
   margin: 0 1rem;
+}
+.carousel{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.leftArrow{
+  margin-right: 1rem;
+}
+.rightArrow{
+  margin-left: 1rem;
 }
 </style>
